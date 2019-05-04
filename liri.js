c2280dc7875707a5acedc5ea7eac5611e780bfec
unknown <pwhynotfilms@gmail.com>
2019-05-03 22:24:11 -0500

    syntax fixed on liri.js

--- liri.js
+++ liri.js
@@ -11,42 +11,42 @@ const userArgument = process.argv[2];
 const input = process.argv.splice(3,process.argv.length).join(' ');
 
 switch (userArgument) {
-  case "spotify-this-song":
+  case "search-music":
     music(input);
     break;
-  case "movies-this":
+  case "search-movies":
     movies(input);
     break;
-  case "concerts-this":
+  case "search-concerts":
     concerts(input);
     break;
-  case "do-what-it-says":
+  case "feeling-lucky":
     doWhatItSays();
     break;
   default:
     Err = "Please enter of the choices below" + "\r\n" + "\r\n" +
-    "node liri.js spotify-this-song '<song name>' " + "\r\n" +
-    "node liri.js movies-this '<movie name>' " + "\r\n" +
-    "node liri.js concerts-this '<favorite artist>' " + "\r\n" +
-    "node liri.js do-what-it-says" + "\r\n" + "\r\n" ; 
+    "node liri.js search-music '<song name>' " + "\r\n" +
+    "node liri.js search-movies '<movie name>' " + "\r\n" +
+    "node liri.js search-concerts '<favorite artist>' " + "\r\n" +
+    "node liri.js feeling-lucky" + "\r\n" + "\r\n" ; 
     console.log(Err)
 };
 
-function doWhatItSays() {
+function feelingLucky() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
         }
         const dataArr = data.split(",");
-        if (dataArr[0] === "spotify-this-song"){
+        if (dataArr[0] === "search-music"){
             const songCheck = dataArr[1].slice(1, -1);
             console.log("Song Check: "+songcheck)
             music(songCheck);
-        } else if (dataArr[0] === "concerts-this"){
+        } else if (dataArr[0] === "search-concerts"){
             const venueName = dataArr[1].slice(1, -1);
             console.log("Venue Name: "+venueName)
             concerts(venueName);
-        } else if (dataArr[0] === "movie-this"){
+        } else if (dataArr[0] === "search-movies"){
             const movieName = dataArr[1].slice(1, -1);
             console.log("Movie Name: "+movieName)
             movies(movieName);
