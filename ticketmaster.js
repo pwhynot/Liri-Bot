87575f711856cec3496e5d6a1a6d523f2d55c4f7
unknown <pwhynotfilms@gmail.com>
2019-05-03 23:09:18 -0500

    syntax changed in ticketmaster.js

--- ticketmaster.js
+++ ticketmaster.js
@@ -5,18 +5,18 @@ const isToday =require("date-fns");
 
 function concerts(input) {
     const artist = input;
-    const url = "http://app.ticketmaster.com/discovery/v1/events.json?keyword=" + artist + keys.ticketmaster.id;
+    const url = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword=" + artist + keys.ticketmaster.id;
 
     axios.get(url).then(
         function(response) {
             for (const i = 0; i < response.data.length; i++){
                 console.log("Concert Time: " + isToday(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
-                console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                console.log("Concert Location: " + response.data[i].venue.city);
                 console.log("Concert Venue: " + response.data[i].venue.name);
                 console.log('--------------------------------------------------')
                 fs.appendFileSync('log.txt', "\r\n" + "Concert Search Log----------------------" + "\r\n", 'utf8');
                 fs.appendFileSync('log.txt', "\r\n" + "Venue Name: " + response.data[i].venue.name + "\r\n", 'utf8');
-                fs.appendFileSync('log.txt', "\r\n" + "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Location: " + response.data[i].venue.city + "\r\n", 'utf8');
                 fs.appendFileSync('log.txt', "\r\n" + "Venue Time: " + isToday(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A') + "\r\n", 'utf8');
                 fs.appendFileSync('log.txt', "\r\n" + "-----------------------------------------"+ "\r\n", 'utf8'); 
             }
